c11bf323f3b12b20de4069b16e94103f6d00c0dc
18757416607 <993323226@qq.com>
2017-12-05 14:25:25 +0800

    開發者密碼

--- src/main/java/com/zldzs/util/Constant.java
+++ src/main/java/com/zldzs/util/Constant.java
@@ -9,7 +9,7 @@ public class Constant {
 
 	public final static String WX_TOKEN = "nhxqtzzldzsdyh";//微信對接tonken
 	
-	public final static String WX_APPSECRET = "7R8EcW5cKZpr95S9owm0cWf65wVBpHnjUrxoNEMdbbw";  //微信開發者密碼
+	public final static String WX_APPSECRET = "1094b8e62bcc4ee65f9aaafda58a2669";  //微信開發者密碼
 	
 	public final static String WX_ENCODINGAESKEY  = "UOcAmTNBSk8P2XOrYU7GkDn8KgjyBZm1hh4OsE8Ed0b"; //消息加解密密钥
 	
