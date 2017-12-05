f9d9897b51f367bd36b93057135724542429f257
18757416607 <993323226@qq.com>
2017-12-05 14:00:41 +0800

    修改常量

--- src/main/java/com/zldzs/util/Constant.java
+++ src/main/java/com/zldzs/util/Constant.java
@@ -7,7 +7,10 @@ package com.zldzs.util;
  */
 public class Constant {
 
-	public final static String WX_TOKEN = "NHXQTZZLDZSDYH";//微信對接tonken
+	public final static String WX_TOKEN = "nhxqtzzldzsdyh";//微信對接tonken
 	
+	public final static String WX_APPSECRET = "7R8EcW5cKZpr95S9owm0cWf65wVBpHnjUrxoNEMdbbw";  //微信開發者密碼
+	
+	public final static String WX_ENCODINGAESKEY  = "UOcAmTNBSk8P2XOrYU7GkDn8KgjyBZm1hh4OsE8Ed0b"; //消息加解密密钥
 	
 }
